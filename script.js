50a2a2c0f5a2bcecfc085d3428cef08fbe1f0612
Ahmed Mamdoh <akremmamdoh@gmail.com>
2025-03-18 09:50:41 +0200

    update
    adding media queries and how to play button

--- script.js
+++ script.js
@@ -11,11 +11,21 @@ const diceEL = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
+const btnHow = document.querySelector('.btn--how-to-play');
+const howModal = document.querySelector('.how-to-play');
+const blurModal = document.querySelector('.blur');
 
 // functions
-const toogleActivePlayer = function () {
+const toggleActivePlayer = function () {
   player0.classList.toggle('player--active');
   player1.classList.toggle('player--active');
+};
+
+// Close Modal function
+
+const closeHowToPlayModal = () => {
+  howModal.classList.add('hidden');
+  blurModal.classList.add('hidden');
 };
 // starting conditions
 score0EL.textContent = 0;
@@ -41,7 +51,8 @@ btnRoll.addEventListener('click', () => {
       current0EL.textContent = 0;
     else current1EL.textContent = 0;
     // Switch to next player
-    toogleActivePlayer();
+    toggleActivePlayer();
+    diceEL.classList.add('hidden');
   }
 });
 
@@ -49,22 +60,28 @@ btnRoll.addEventListener('click', () => {
 btnHold.addEventListener('click', () => {
   // Add current score to the active player's score
   if (player0.classList.contains('player--active')) {
+    // cant hold on 0
+    if (current0EL.textContent == 0) return;
+    diceEL.classList.add('hidden');
     score0EL.textContent =
       Number(score0EL.textContent) + Number(current0EL.textContent);
     current0EL.textContent = 0;
   } else {
+    if (current1EL.textContent == 0) return;
+    diceEL.classList.add('hidden');
+
     score1EL.textContent =
       Number(score1EL.textContent) + Number(current1EL.textContent);
     current1EL.textContent = 0;
   }
 
-  if (score0EL.textContent >= 20 || score1EL.textContent >= 20) {
+  if (score0EL.textContent >= 50 || score1EL.textContent >= 50) {
     document.querySelector('.player--active').classList.add('player--winner');
     diceEL.classList.add('hidden');
     btnRoll.disabled = true;
     btnHold.disabled = true;
   } else {
-    toogleActivePlayer();
+    toggleActivePlayer();
   }
 });
 
@@ -77,6 +94,22 @@ btnNew.addEventListener('click', () => {
   diceEL.classList.add('hidden');
   btnRoll.disabled = false;
   btnHold.disabled = false;
-  document.querySelector('.player--winner').classList.remove('player--winner');
-  if (player1.classList.contains('player--active')) toogleActivePlayer();
+  document.querySelector('.player--winner')?.classList.remove('player--winner');
+  if (player1.classList.contains('player--active')) toggleActivePlayer();
+});
+
+// How to play modal functionality
+
+btnHow.addEventListener('click', () => {
+  howModal.classList.remove('hidden');
+  blurModal.classList.remove('hidden');
+});
+
+// Close modal
+blurModal.addEventListener('click', () => {
+  closeHowToPlayModal();
+});
+
+document.getElementsByTagName('body')[0].addEventListener('keydown', e => {
+  if (e.key === 'Escape') closeHowToPlayModal();
 });
